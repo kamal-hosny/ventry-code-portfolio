919b95a6874c2d47ddf884e79e3d9603312ffd83
kamal <kamal@gmail.com>
2024-08-24 20:04:49 +0300

    fix

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -5,14 +5,13 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://7b883d075caa19b41fd9b00ae313a1c6@o4506813739368448.ingest.us.sentry.io/4507222371729408",
+  dsn: "https://961555d93b691bfd77e9b2240a3a5a0d@o4507827788251136.ingest.de.sentry.io/4507832493604944",
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
-
   replaysOnErrorSampleRate: 1.0,
 
   // This sets the sample rate to be 10%. You may want this to be 100% while
@@ -25,10 +24,6 @@ Sentry.init({
       // Additional Replay configuration goes in here, for example:
       maskAllText: true,
       blockAllMedia: true,
-    }),
-    Sentry.feedbackIntegration({
-      // Additional SDK configuration goes in here, for example:
-      colorScheme: "system",
     }),
   ],
 });
