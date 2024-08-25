55389b792d7d2c9a195a489f920340eb52e52653
kamal <kamal@gmail.com>
2024-08-25 14:31:20 +0300

    update

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -8,14 +8,14 @@ import { PinContainer } from "./ui/Pin";
 const RecentProjects = () => {
   return (
     <div className="py-20">
-      <h1 className="heading">
+      <h1 className="heading text-center px-4">
         A small selection of{" "}
         <span className="text-purple">recent projects</span>
       </h1>
-      <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
+      <div className="grid gap-8 mt-10 px-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
         {projects.map((item) => (
           <div
-            className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]"
+            className="h-[25rem] flex items-center justify-center w-full max-w-[20rem] lg:max-w-[24rem] xl:max-w-[28rem]"
             key={item.id}
           >
             <a
@@ -24,13 +24,17 @@ const RecentProjects = () => {
               rel="noopener noreferrer"
               className="block w-full h-full"
             >
-              <PinContainer title="Visit the site" href={item.link}>
-                <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
+              <div className="border-[#141729] border-2 p-4 rounded-lg h-full flex flex-col justify-between">
+                <div className="relative flex items-center justify-center overflow-hidden h-[20vh] lg:h-[30vh] mb-4">
                   <div
                     className="relative w-full h-full overflow-hidden lg:rounded-3xl"
                     style={{ backgroundColor: "#13162D" }}
                   >
-                    <img src="/bg.png" alt="bgimg" />
+                    <img
+                      src="/bg.png"
+                      alt="bgimg"
+                      className="w-full h-full object-cover"
+                    />
                   </div>
                   <img
                     src={item.img}
@@ -39,43 +43,34 @@ const RecentProjects = () => {
                   />
                 </div>
 
-                <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
+                <h1 className="font-bold text-lg lg:text-xl xl:text-2xl line-clamp-1">
                   {item.title}
                 </h1>
 
-                <p
-                  className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2"
-                  style={{
-                    color: "#BEC1DD",
-                    margin: "1vh 0",
-                  }}
-                >
+                <p className="text-sm text-[#BEC1DD] mt-2 mb-4 line-clamp-2">
                   {item.des}
                 </p>
 
-                <div className="flex items-center justify-between mt-7 mb-3">
-                  <div className="flex items-center">
+                <div className="flex items-center justify-between">
+                  <div className="flex items-center -space-x-2">
                     {item.iconLists.map((icon, index) => (
                       <div
                         key={index}
-                        className="border border-white/[.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
-                        style={{
-                          transform: `translateX(-${5 * index + 2}px)`,
-                        }}
+                        className="border border-white/[.2] rounded-full bg-black w-8 h-8 lg:w-10 lg:h-10 flex justify-center items-center"
                       >
-                        <img src={icon} alt="icon5" className="p-2" />
+                        <img src={icon} alt="icon5" className="p-1 w-3/4" />
                       </div>
                     ))}
                   </div>
 
-                  <div className="flex justify-center items-center">
-                    <p className="flex lg:text-xl md:text-xs text-sm text-purple">
+                  <div className="flex items-center">
+                    <p className="text-sm  text-purple">
                       Check Live Site
                     </p>
-                    <FaLocationArrow className="ms-3" color="#CBACF9" />
+                    <FaLocationArrow className="ml-2 lg:ml-3" color="#CBACF9" />
                   </div>
                 </div>
-              </PinContainer>
+              </div>
             </a>
           </div>
         ))}
