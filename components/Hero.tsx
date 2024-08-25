4d4f07ec1c620c70cbf3228b5a7d79be48c0cddd
kamal <kamal@gmail.com>
2024-08-25 12:14:38 +0300

    ss

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -19,12 +19,12 @@ const Hero = () => {
         <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
       </div>
       <div
-        className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
+        className="h-screen w-full bg-black-100 dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
        absolute top-0 left-0 flex items-center justify-center"
       >
         <div
-          className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100
-         bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
+          className="absolute pointer-events-none inset-0 flex items-center justify-center bg-black-100
+         [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
         />
       </div>
 
